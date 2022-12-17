74fd16847519c82699631f7dc6f811f96189119a
Melike <70147969+MelikeCigdem@users.noreply.github.com>
2022-12-18 01:24:27 +0300

    react list

--- react-list/src/pages/home/Home.js
+++ react-list/src/pages/home/Home.js
@@ -1,10 +1,133 @@
-import './Home.module.css';
-import { Container, Paper } from '@mui/material';
+import './Home.css';
+import { Paper, Container } from '@mui/material';
+import GradeIcon from '@mui/icons-material/Grade';
+import {useEffect, useState} from "react";
+import axios from "axios";
+import { styled } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
+
 
 export default function Home(){
+    const [datas, setDatas] = useState(null);
+    const [like, setLike] = useState("#b9b1b1")
+    const config = {
+        method: 'get',
+        url: 'https://assignment-api.piton.com.tr/api/v1/product/all',
+        headers: {
+            'accept': 'application/json',
+            'access-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InN0cmluZyIsImlhdCI6MTY0OTg2MDExMiwiZXhwIjoxNjc1NzgwMTEyfQ.z8XYELsP1GBKkGpyvI14WzJKQAAbtQUwCl3hlLs_U4M'
+        },
+        data : ''
+    };
+    useEffect(()=>{
+        axios(config)
+            .then(function (response) {
+                setDatas(response.data.products);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+
+    },[]);
+    const Item = styled(Paper)(({ theme }) => ({
+        backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+        ...theme.typography.body2,
+        padding: theme.spacing(1),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    }));
+    function addLikes(id){
+        console.log(id);
+        setLike("#ffd700");
+
+            var myHeaders = new Headers();
+            myHeaders.append("accept", "application/json");
+            myHeaders.append("access-token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InN0cmluZyIsImlhdCI6MTY0OTg2MDExMiwiZXhwIjoxNjc1NzgwMTEyfQ.z8XYELsP1GBKkGpyvI14WzJKQAAbtQUwCl3hlLs_U4M");
+            myHeaders.append("Content-Type", "application/json");
+
+            var raw = JSON.stringify({
+                "productId": id
+            });
+
+            var requestOptions = {
+                method: 'POST',
+                headers: myHeaders,
+                body: raw,
+                redirect: 'follow'
+            };
+
+            fetch("https://assignment-api.piton.com.tr/api/v1/product/like", requestOptions)
+                .then(response => response.text())
+                .then(result => {
+                    if(result.status == "Success"){
+                        console.log(result);
+
+                        console.log(like)
+                    }
+                })
+                .catch(error => console.log('error', error));
+
+    }
     return(
-        <Container sx={{mt:8}}>
-            <Paper>Liste</Paper>
+        <Container sx={{mt:12}}>
+            <Grid container spacing={2}>
+                <Grid item xs={2}>
+                    <Item>Name</Item>
+                </Grid>
+                <Grid item xs={2}>
+                    <Item>Price</Item>
+                </Grid>
+                <Grid item xs={2}>
+                    <Item>Image</Item>
+                </Grid>
+                <Grid item xs={2}>
+                    <Item>TimeStamp</Item>
+                </Grid>
+                <Grid item xs={2}>
+                    <Item>Description</Item>
+                </Grid>
+                <Grid item xs={1}>
+                    <Item>Likes</Item>
+                </Grid>
+                <Grid item xs={1}>
+
+                </Grid>
+            </Grid>
+
+            <div>
+                {datas && datas.map((obj)=>(
+                    <Grid container spacing={2} sx={{ my: 0 }} key={obj.id}>
+                        <Grid item xs={2}  >
+                            <div className="table-body">{obj.name}</div>
+                        </Grid>
+                        <Grid item xs={2}>
+                            <div className="table-body">{obj.price}</div>
+                        </Grid>
+                        <Grid item xs={2}>
+                            <div className="table-body">{obj.image}</div>
+                        </Grid>
+                        <Grid item xs={2}>
+                            <div className="table-body">{obj.timeStamp}</div>
+                        </Grid>
+                        <Grid item xs={2}>
+                            <div className="table-body">{obj.description}</div>
+                        </Grid>
+                        <Grid item xs={1}>
+                            <div className="table-body">{obj.likes}</div>
+                        </Grid>
+                        <Grid item xs={1}>
+                            <div className="table-body">
+                                <GradeIcon style={{ color: like }} onClick={() => addLikes(obj.id)} color="primary" />
+                            </div>
+                        </Grid>
+                    </Grid>
+                ))}
+
+
+            </div>
+
         </Container>
+
+
     )
 }
